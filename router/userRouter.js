70df6c1a535fe7cf3c014d7dc5b9a87263df5198
WonderXuy <veko.dmitriy0@gmail.com>
2020-03-20 02:00:31 +0300

    express-server/feat: add get router

--- router/userRouter.js
+++ router/userRouter.js
@@ -3,4 +3,8 @@ const express = require('express');
 const UserController = require('../controllers/user-controller')
 const user_controller = new UserController();
 
-const router = new express.Router()
+const router = new express.Router();
+
+router.get('/:id', user_controller.getUser);
+
+module.exports = router;
